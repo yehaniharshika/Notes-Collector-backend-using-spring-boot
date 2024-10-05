588493677415028e6d58fa23d7de1897ae84c8fc
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-10-05 14:00:55 +0530

    adding properties file and adding context path

--- src/main/java/lk/ijse/notecollectorusingspringboot/NoteCollectorUsingSpringbootApplication.java
+++ src/main/java/lk/ijse/notecollectorusingspringboot/NoteCollectorUsingSpringbootApplication.java
@@ -1,13 +1,20 @@
 package lk.ijse.notecollectorusingspringboot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NoteCollectorUsingSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteCollectorUsingSpringbootApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
